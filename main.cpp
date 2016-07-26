a128436b3e2a1f13dff1fab3bc44f969c7d1908c
Ryan <lyan12@jhu.edu>
2016-07-26 20:48:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <rtt/os/main.h>
 
 #include <iostream>
-#include "two_components.hpp"
+#include "xb6recvcomponent.hpp"
 #include "xb6pubcomponent.hpp"
 
 using namespace RTT;
@@ -13,24 +13,24 @@ using namespace std;
 int ORO_main(int argc, char** argv){
 
 	xb6pubcomponent xb6pub("xb6pub");
-	Hello hello("hello");
+	xb6recvcomponent xb6recv("xb6recv");
 
-	connectPeers(&hello, &xb6pub);
+	connectPeers(&xb6recv, &xb6pub);
 
 	xb6pub.setPeriod(0.002);
-	hello.setPeriod(0.002);
+	xb6recv.setPeriod(0.002);
 
 	Logger::Instance()->setLogLevel(Logger::Info);
 
-	xb6pub.getPort("xb6_pos_cmd")->connectTo(hello.getPort("input_pos_cmd"));
-	hello.getPort("output_setpoint")->connectTo(xb6pub.getPort("xb6_pos_current"));
+	xb6pub.getPort("xb6_pos_cmd")->connectTo(xb6recv.getPort("input_pos_cmd"));
+	xb6recv.getPort("output_setpoint")->connectTo(xb6pub.getPort("xb6_pos_current"));
 
 	cout<<xb6pub.getPort("xb6_pos_cmd")->connected()<<endl;
-	cout<<hello.getPort("output_setpoint")->connected()<<endl;
+	cout<<xb6recv.getPort("output_setpoint")->connected()<<endl;
 
-	if(!hello.start()){
-		hello.configure();
-		hello.start();
+	if(!xb6recv.start()){
+		xb6recv.configure();
+		xb6recv.start();
 	}
 	int res = xb6pub.configure();
 	cout<<res<<endl;
